8f02191145448c13fdb8be02e5f6e0131b912e86
morgankokicada <morgan.kokic@ada.ac.uk>
2022-11-04 12:23:27 +0000

    difference from answer function working

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ num = 0
 def generate_random_num():
   return random.randint(0, 10)
 
+
+def diff_from_answer(guess, ans):
+  diff = ans - guess
+  if diff == 0:
+    return "Correct"
+  elif diff > 0:
+    return "Too low"
+  else:
+    return "Too high"
